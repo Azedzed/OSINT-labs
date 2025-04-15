6e4e95b82725b3c853466676cca48da7e74bb126
kali <kali@kali.kali>
2025-04-15 13:40:48 -0400

    Bare minimum so far

---
+++ scraper.py
@@ -0,0 +1,18 @@
+import time
+import requests
+import bs4
+from bs4 import BeautifulSoup
+
+url = "https://pastebin.com/archive"
+
+resposne = requests.get(url)
+
+soup = BeautifulSoup(repsonse.text, 'html.parser')
+
+usernames = soup.find_all("div", class_="username")
+
+for username in usernames:
+    username_text = username.find('a').text.strip()
+    print(username_text)
+
+    
